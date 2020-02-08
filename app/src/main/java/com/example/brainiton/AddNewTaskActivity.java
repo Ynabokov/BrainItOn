665185a916350c07e1819e273559befe16cfe63c
ewong012 <58529979+ewong012@users.noreply.github.com>
2020-02-08 13:54:30 -0800

    Changed date input to use date picker widget

--- app/src/main/java/com/example/brainiton/AddNewTaskActivity.java
+++ app/src/main/java/com/example/brainiton/AddNewTaskActivity.java
@@ -1,33 +1,62 @@
 package com.example.brainiton;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Date;
-
+import java.util.Calendar;
 
 public class AddNewTaskActivity extends AppCompatActivity {
 
     String name;
     String due_date;
 
+    EditText nameInput;
+    EditText dateInput;
+    Button addTaskButton;
+
+    DatePickerDialog datePicker;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_task);
 
+        nameInput = findViewById(R.id.inputName);
+        dateInput = findViewById(R.id.inputDueDate);
+        addTaskButton = findViewById(R.id.createNewTaskButton);
+
+        dateInput.setInputType(InputType.TYPE_NULL);
+        dateInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                datePicker = new DatePickerDialog(AddNewTaskActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                dateInput.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                datePicker.show();
+            }
+        });
     }
 
     public void createTask(View v) {
-        final TextView inputName = findViewById(R.id.inputName);
-        final TextView inputDueDate = findViewById(R.id.inputDueDate);
-        name = inputName.getText().toString();
-        //due_date = inputDueDate.getText().toString().split("-");
-        due_date = inputDueDate.getText().toString();
-        Task new_task = new Task(name, due_date);
-        // send to main view
+        //Task new_task = new Task(name, due_date);
+
+        // send back to main view
     }
 }
